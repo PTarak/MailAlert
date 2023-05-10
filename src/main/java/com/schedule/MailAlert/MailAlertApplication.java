c4ddfe997930de19dce1e144baa4b9ef0d88ec89
Tarak Potturi <potturitarakanadhareddy@gmail.com>
2023-05-10 14:21:06 +0530

    Initial commit of the project having skeleton

---
+++ src/main/java/com/schedule/MailAlert/MailAlertApplication.java
@@ -0,0 +1,13 @@
+package com.schedule.MailAlert;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MailAlertApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MailAlertApplication.class, args);
+	}
+
+}
